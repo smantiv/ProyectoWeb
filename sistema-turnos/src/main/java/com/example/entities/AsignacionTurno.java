bd39d2017b70c0daeaa0a1a969e3035c41d3ac8b
Isabella Palacio Fajardo <palaciofajardoisabella@gmail.com>
2026-03-15 14:15:39 -0500

    actualizacion de las diferentes clases de entiti

--- sistema-turnos/src/main/java/com/example/entities/AsignacionTurno.java
+++ sistema-turnos/src/main/java/com/example/entities/AsignacionTurno.java
@@ -1,5 +1,80 @@
 package com.example.entities;
 
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "asignacion_turno")
 public class AsignacionTurno {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDateTime horaCheckin;
+    private LocalDateTime horaCierre;
+    private Integer calificacionLimpieza;
+    private String estadoCobertura;
+
+    @ManyToOne
+    @JoinColumn(name = "docente_id")
+    private Docente docente;
+
+    @ManyToOne
+    @JoinColumn(name = "turno_id")
+    private Turno turno;
+
+    public AsignacionTurno() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public LocalDateTime getHoraCheckin() {
+        return horaCheckin;
+    }
+
+    public void setHoraCheckin(LocalDateTime horaCheckin) {
+        this.horaCheckin = horaCheckin;
+    }
+
+    public LocalDateTime getHoraCierre() {
+        return horaCierre;
+    }
+
+    public void setHoraCierre(LocalDateTime horaCierre) {
+        this.horaCierre = horaCierre;
+    }
+
+    public Integer getCalificacionLimpieza() {
+        return calificacionLimpieza;
+    }
+
+    public void setCalificacionLimpieza(Integer calificacionLimpieza) {
+        this.calificacionLimpieza = calificacionLimpieza;
+    }
+
+    public String getEstadoCobertura() {
+        return estadoCobertura;
+    }
+
+    public void setEstadoCobertura(String estadoCobertura) {
+        this.estadoCobertura = estadoCobertura;
+    }
+
+    public Docente getDocente() {
+        return docente;
+    }
+
+    public void setDocente(Docente docente) {
+        this.docente = docente;
+    }
+
+    public Turno getTurno() {
+        return turno;
+    }
+
+    public void setTurno(Turno turno) {
+        this.turno = turno;
+    }
 }
