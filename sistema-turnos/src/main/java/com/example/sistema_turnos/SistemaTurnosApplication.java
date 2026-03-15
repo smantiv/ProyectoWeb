86592754545312316858e34cf49f5c750506e850
Isabella Palacio Fajardo <palaciofajardoisabella@gmail.com>
2026-03-15 12:32:49 -0500

    Estructura inicial del proyecto Spring Boot

---
+++ sistema-turnos/src/main/java/com/example/sistema_turnos/SistemaTurnosApplication.java
@@ -0,0 +1,13 @@
+package com.example.sistema_turnos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaTurnosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaTurnosApplication.class, args);
+	}
+
+}
---
+++ sistema-turnos/src/test/java/com/example/sistema_turnos/SistemaTurnosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sistema_turnos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaTurnosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
